426422c8a70dd8728d75918aa9ed43713126aa00
Till Toenshoff <toenshoff@me.com>
2024-02-20 19:32:29 +0100

    Adds omitted POI header

---
+++ PlayEm/ProfilingPointsOfInterest.h
@@ -0,0 +1,32 @@
+//
+//  ProfilingPointsOfInterest.h
+//  PlayEm
+//
+//  Created by Till Toenshoff on 20.02.24.
+//  Copyright © 2024 Till Toenshoff. All rights reserved.
+//
+
+#import <OSLog/OSLog.h>
+
+#ifndef ProfilingPointsOfInterest_h
+#define ProfilingPointsOfInterest_h
+
+typedef enum : NSUInteger {
+    POICADisplayLink,
+    POISetCurrentFrame,
+    POIVisualsFromOrigin,
+    POIGetCurrentFrame,
+    POIAudioBufferCallback,
+    POIUpdateScrollingState,
+    POIUpdateHeadPosition,
+    POIScrollPoint,
+    POIStringStuff,
+    POIWaveViewSetCurrentFrame,
+    POITotalViewSetCurrentFrame,
+    POIBeatStuff,
+    POIPrepareVisualsFromOrigin,
+} PointsOfInterest;
+
+extern os_log_t pointsOfInterest;
+
+#endif /* ProfilingPointsOfInterest_h */

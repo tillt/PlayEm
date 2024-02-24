4c1152cf70a3895291b2342907d3545c165985cb
Till Toenshoff <toenshoff@me.com>
2024-02-24 02:08:30 +0100

    now on 1.2

--- PlayEm/ScopeRenderer.h
+++ PlayEm/ScopeRenderer.h
@@ -17,22 +17,18 @@ NS_ASSUME_NONNULL_BEGIN
 @class NSColor;
 
 @protocol ScopeRendererDelegate <NSObject>
-
 - (VisualSample*)visualSample;
 - (AudioController*)audioController;
-
 @end
 
 
 @interface ScopeRenderer : NSObject <MTKViewDelegate>
 
 @property (weak, nonatomic) NSLevelIndicator* level;
-
 @property (assign, nonatomic) unsigned long long frames;
 @property (assign, nonatomic) unsigned long long currentFrame;
 
 - (nonnull instancetype)initWithMetalKitView:(nonnull MTKView *)view color:(NSColor *)color fftColor:(NSColor *)fftColor background:(NSColor *)background delegate:(id<ScopeRendererDelegate>)delegate;
-
 - (void)play:(nonnull AudioController *)audio visual:(nonnull VisualSample *)visual scope:(nonnull MTKView *)scope;
 - (void)stop:(MTKView *)scope;
 

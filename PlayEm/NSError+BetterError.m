47b52c44e156b7229f8bde9a0591d0f159123448
Till Toenshoff <toenshoff@me.com>
2024-02-27 20:52:13 +0100

    finetuned the error messaging

--- PlayEm/NSError+BetterError.m
+++ PlayEm/NSError+BetterError.m
@@ -5,6 +5,7 @@
 //  Created by Till Toenshoff on 27.02.24.
 //  Copyright © 2024 Till Toenshoff. All rights reserved.
 //
+#include <ctype.h>
 
 #import "NSError+BetterError.h"
 
@@ -14,14 +15,19 @@
 
 + (NSString*)stringFromOSStatus:(NSInteger)code
 {
-    char cString[5];
-    
-    cString[0] = (code >> 24) & 0xFF;
-    cString[1] = (code >> 16) & 0xFF;
-    cString[2] = (code >>  8) & 0xFF;
-    cString[3] = code         & 0xFF;
-    cString[4] = 0;
-    
+    const int length = 4;
+    char cString[length + 1];
+
+    for (int i = 0; i < length; i++) {
+        int shift = (length - (i + 1)) * 8;
+        int component = (code >> shift) & 0xFF;
+        if (component == 0 || !isascii(component)) {
+            return nil;
+        }
+        cString[i] = component;
+    }
+    cString[length] = 0;
+  
     return [NSString stringWithCString:cString encoding:NSStringEncodingConversionAllowLossy];
 }
 
@@ -54,48 +60,61 @@
         @(kAudioFileNotOptimizedError): @"not optimized",
         @(kAudioFileInvalidChunkError): @"invalid chunk",
         @(kAudioFileDoesNotAllow64BitDataSizeError): @"does not allow 64bit data size",
-        @(kAudioFileInvalidPacketOffsetError): @"invalid packat offset",
+        @(kAudioFileInvalidPacketOffsetError): @"invalid packet offset",
         @(kAudioFileInvalidPacketDependencyError): @"packet dependency error",
         @(kAudioFileInvalidFileError): @"invalid file error",
         @(kAudioFileOperationNotSupportedError): @"operation not supported",
     };
 
     NSString* description = error.localizedDescription;
-    NSString* extendedDescription = stringMap[@(error.code)];
     NSString* failureReason = error.localizedFailureReason;
     NSString* fileName = [[url filePathURL] lastPathComponent];
 
+    // Reduce the hard to read fully qualified framework name into the last component.
     NSArray<NSString*>* frameworkComponents = [error.domain componentsSeparatedByString:@"."];
     NSString* frameworkName = frameworkComponents[frameworkComponents.count - 1];
+
+    // Remove the framework error code prefix from the description. Example "(com.apple.avfaudio error XXXXXXXX)".
     NSString* frameworkErrorCodePrefix = [NSString stringWithFormat:@"(%@ error ", error.domain];
     NSRange range = [description rangeOfString:frameworkErrorCodePrefix];
-
-    if (action == nil) {
-        action = @"process";
-    }
-    NSString* urlInsert = @"";
-    if (fileName != nil) {
-        urlInsert = [NSString stringWithFormat:@" the file \'%@\'", fileName];
-    }
-
-    // Spare the user the hard to read framework name.
     if (range.length > 0) {
         NSString* goodPart = [description substringToIndex:range.location];
-        NSString* trigraph = [NSError stringFromOSStatus:error.code];
-        
-        if (failureReason == nil) {
-            failureReason = [NSString stringWithFormat:@"When trying to %@%@ %@ returned: '%@'",
-                             action,
-                             urlInsert,
-                             frameworkName,
-                             extendedDescription];
-        }
         description = goodPart;
     }
+
     if (failureReason == nil) {
-        failureReason = [NSString stringWithFormat:@"Tried to %@ \"%@\"", action, fileName];
+        // Set a default action.
+        if (action == nil) {
+            action = @"process";
+        }
+
+        // Make sure the file we were trying to access is mentioned.
+        NSString* urlInsert = @"";
+        if (fileName != nil && ![description containsString:fileName]) {
+            urlInsert = [NSString stringWithFormat:@" \"%@\"", fileName];
+        }
+
+        // Add the trigraph from the error code (OSStatus) if available.
+        NSString* trigraphInsert = @"";
+        NSString* trigraph = [NSError stringFromOSStatus:error.code];
+        if (trigraph != nil) {
+            trigraphInsert = [NSString stringWithFormat:@" (%@)", trigraph];
+        }
+        
+        // Add a description derived from the hopefully known error code.
+        NSString* extendedDescription = stringMap[@(error.code)];
+        if (extendedDescription == nil) {
+            extendedDescription = @"";
+        }
+
+        // Build a neat failureReason.
+        failureReason = [NSString stringWithFormat:@"When trying to %@%@ %@ suggests: %@%@.",
+                         action,
+                         urlInsert,
+                         frameworkName,
+                         extendedDescription,
+                         trigraphInsert];
     }
-    failureReason = [failureReason stringByAppendingString:@"."];
     
     NSDictionary* userInfo = @{
         NSLocalizedDescriptionKey: description,

39e5b0c4db81a568ddaf49df6615e52fec988054
Till Toenshoff <toenshoff@me.com>
2024-12-30 20:55:18 +0100

    fix: we should be solid now on resize

--- PlayEm/ScopeRenderer.m
+++ PlayEm/ScopeRenderer.m
@@ -812,22 +812,21 @@ static const double kLevelDecreaseValue = 0.042;
 /// Per frame updates.
 - (void)drawInMTKView:(nonnull MTKView *)view
 {
-    dispatch_semaphore_wait(_inFlightSemaphore, DISPATCH_TIME_FOREVER);
-    
-    id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
-#ifdef DEBUG_METAL_RESOURCE_LABELS
-    commandBuffer.label = @"Scope Command Buffer";
-#endif
-    __block dispatch_semaphore_t block_sema = _inFlightSemaphore;
-    [commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) {
-        dispatch_semaphore_signal(block_sema);
-    }];
-    
-    [self _updateDynamicBufferState];
-    [self _updateEngine];
     
     dispatch_sync(_renderQueue, ^{
-    {
+        dispatch_semaphore_wait(_inFlightSemaphore, DISPATCH_TIME_FOREVER);
+        
+        id<MTLCommandBuffer> commandBuffer = [_commandQueue commandBuffer];
+    #ifdef DEBUG_METAL_RESOURCE_LABELS
+        commandBuffer.label = @"Scope Command Buffer";
+    #endif
+        __block dispatch_semaphore_t block_sema = _inFlightSemaphore;
+        [commandBuffer addCompletedHandler:^(id<MTLCommandBuffer> buffer) {
+            dispatch_semaphore_signal(block_sema);
+        }];
+        
+        [self _updateDynamicBufferState];
+        [self _updateEngine];
         // First pass rendering code: Drawing the scope line.
         
         id<MTLRenderCommandEncoder> renderEncoder = [commandBuffer renderCommandEncoderWithDescriptor:_scopePass];
@@ -856,7 +855,6 @@ static const double kLevelDecreaseValue = 0.042;
                         instanceCount:(_sampleCount + (_sampleStepping - 1))/ _sampleStepping];
         
         [renderEncoder endEncoding];
-    }
     
         [_bloom encodeToCommandBuffer:commandBuffer
                         sourceTexture:_scopeTargetTexture

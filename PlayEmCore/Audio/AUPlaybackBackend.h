ac72d9e53ac58967bfa21c0e3cd00dc1ae412257
Till Toenshoff <toenshoff@me.com>
2026-01-07 22:02:21 +0100

    feat: audio units playback interface

---
+++ PlayEmCore/Audio/AUPlaybackBackend.h
@@ -0,0 +1,15 @@
+//
+//  AUPlaybackBackend.h
+//  PlayEm
+//
+//  Created by Till Toenshoff on 01/05/26.
+//  Copyright © 2026 Till Toenshoff. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "AudioPlaybackBackend.h"
+
+@interface AUPlaybackBackend : NSObject <AudioPlaybackBackend>
+
+@end
